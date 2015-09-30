22f525d9110ad9d3b2e1d92737ca3fac4aa07ee7
Bakiraka <anthony.djenane@gmail.com>
2015-09-30 16:18:47 +0200

    correction génération facture

--- merchant_verif_invoice_n_check.py
+++ merchant_verif_invoice_n_check.py
@@ -9,6 +9,9 @@
 ####    Output : Either that the check is fine or not          ####
 ###################################################################
 import sys
+import FileUtils
+import Facture
+import RSAtools
 
 if( len(sys.argv) < 4 ):
     print("You need to put all of the arguments (invoice file, signed check and client's Pk)")
@@ -17,8 +20,27 @@ if( len(sys.argv) < 4 ):
 # file opening
 try:
     invoice_file = open(sys.argv[1], 'r')
-    invoice_file = open(sys.argv[2], 'r')
-    invoice_file = open(sys.argv[3], 'r')
+    signedcheck_file = open(sys.argv[2], 'r')
+    clientkey_file = open(sys.argv[3], 'r')
 except (OSError, IOError) as error:
     print("Error reading file : ", error)
     sys.exit()
+
+#Getting the infos from the files
+clepub_client = FileUtils.recupKey(sys.argv[3])
+
+facture = Facture(sys.argv[1])
+
+clepub_client_bankciphered = signedcheck_file.readline()
+sum_n_id_ciphered = signedcheck_file.readline()
+
+ciphered_check_w_clientpk = ""
+for lines in signedcheck_file:
+    ciphered_check_w_clientpk += lines
+
+sum_n_id_clear = decrypt_pk_str(clepub_client, sum_n_id_ciphered)
+sum_n_id = sum_n_id_clear.split(" ")
+if(sum_n_id[0] != facture.getUid()):
+    print
+if(sum_n_id[1] != facture.getTotalSomme()):
+    print
