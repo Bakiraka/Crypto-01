e1e9494a963491b1a2386ee94973ada044b63710
Bakiraka <anthony.djenane@gmail.com>
2015-10-04 15:40:44 +0200

    Vérification du chèque de la banque
    Il manque la vérification de si le chèque n'a pas déjà été encaissé
    (fichier json ?)

--- bank_check.py
+++ bank_check.py
@@ -10,12 +10,15 @@
 ####                    verify checks                          ####
 ####    Output : Either that the check is fine or not          ####
 ###################################################################
+import FileUtils
+import RSAtools
 
+#Checking the number of arguments for the optional file
 checkfile_specified = False
 if( len(sys.argv) >= 5):
     checkfile_specified = True
 
-#file opening
+#Making sure the arguments and the files are there
 try:
     check_file = open(sys.argv[1], 'r')
     clientkey_file = open(sys.argv[2], 'r')
@@ -27,3 +30,34 @@ try:
 except (OSError, IOError) as error:
     print("Error reading file : ", error)
     sys.exit()
+finally:
+    checkfile.close()
+    clientkey_file.close()
+    merchantkey_file.close()
+
+#Getting client's key and merchant's original key
+clientkey_original = FileUtils.recupKey(sys.argv[2])
+merchant_key_original = FileUtils.recupKey(sys.argv[3])
+#Getting merchant's key ciphered by the client
+merchant_key_ciphered = check_file.readline()
+#Deciphering
+merchant_key_deciphered = RSAtools.decrypt( clientkey_original, merchant_key_ciphered)
+#Verification
+if( merchant_key_deciphered != merchant_key_original):
+    print("La clé du marchant dans le chèque n'est pas la même que celle fournie !\n")
+
+#Getting client's key ciphered by the bank
+clientkey_ciphered = checkfile.readline()
+#Getting the bank's private key
+bank_privatekey = FileUtils.recupKey('bankPk')
+#Deciphering the client's key
+clientkey_deciphered = RSAtools.decrypt( bank_privatekey, clientkey_ciphered)
+#Verification
+if( clientkey_deciphered != clientkey_original):
+    print("La clé du client dans le chèque n'est pas la même que celle fournie !\n")
+
+#Getting the ciphered uid and sum from the check
+uidciphered = checkfile.readline()
+sumciphered = checkfile.readline()
+
+#Making sure the check hasn't already been cashed
